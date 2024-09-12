84b84e99bd06f22aec9c08e1eb1b3713bd6638fa
Caroline Muscara <cjm72@bu.edu>
2024-09-12 14:19:09 -0400

    filled in missing code

--- test_utils.py
+++ test_utils.py
@@ -13,19 +13,20 @@ def test_dot_product():
     assert result == 32, f"Expected 32, but got {result}"
     
 def test_cosine_similarity():
-    ### YOUR CODE HERE
+    vector1 = np.array([1, 2, 3])
+    vector2 = np.array([4, 5, 6])
+
+    result = cosine_similarity(vector1, vector2)
     
-    result = ### YOUR CODE HERE
-    
-    expected_result = ### YOUR CODE HERE
+    expected_result = 0.9746318461970762
     
     assert np.isclose(result, expected_result), f"Expected {expected_result}, but got {result}"
 
 def test_nearest_neighbor():
-    ### YOUR CODE HERE
+    vectors = np.array([[1, 2], [3, 4], [5, 6]])
+    target_vector = np.array([2,3])
+    result = nearest_neighbor(vectors, target_vector)
     
-    result = ### YOUR CODE HERE
-    
-    expected_index = ### YOUR CODE HERE
+    expected_index = 0
     
     assert result == expected_index, f"Expected index {expected_index}, but got {result}"
